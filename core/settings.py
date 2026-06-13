baab399a44f42a8385fc2ef80fbd6aa37c54fa7b
Azizbek <norboyevazizbek54@gmail.com>
2026-06-13 16:03:03 +0500

    fix allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-b@h44%)is0((xrdwymzl_i+0mn%j4&x=4d!xif-u4ic0j0)!3f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
